7c13712ea6b8a172280c570cd0f7d7388c6f0663
Maya Mill <mayam514@bmw35.cs.colman.ac.il>
2018-01-14 20:02:51 +0200

    changes in model file for chat

---
+++ Dshare/Dshare/Model/ConversationFirebase.swift
@@ -0,0 +1,9 @@
+//
+//  ConversationFirebase.swift
+//  Dshare
+//
+//  Created by Maya Mill on 14/01/2018.
+//  Copyright © 2018 Munoz, Valentina. All rights reserved.
+//
+
+import Foundation
---
+++ Dshare/Dshare/Model/MessagesHandler.swift
@@ -0,0 +1,9 @@
+//
+//  MessagesHandler.swift
+//  Dshare
+//
+//  Created by Maya Mill on 14/01/2018.
+//  Copyright © 2018 Munoz, Valentina. All rights reserved.
+//
+
+import Foundation
